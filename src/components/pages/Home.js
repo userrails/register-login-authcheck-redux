3ec8e3b694555e0a47c205e36423a8772ccec843
Shiv Raj <shivrajbadu@gmail.com>
2019-06-27 18:18:22 +0545

    To see home stuff, just login

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,27 @@
 import React, {Component} from 'react';
+import fakeAuth from '../../containers/fakeAuth';
+import { connect } from "react-redux";
 
 class Home extends Component {
   render () {
-    return (
-      <div>Home: Welcome to my Home page.</div>
-    )
+    if (this.props.isAuthenticated===true) {
+      return (
+          <div>
+            <h1>This is my Home page.</h1>
+          </div>
+        )
+      } else {
+      return (
+          <p>You are not yet loggedin, first <a href="/register">Register</a> your account.</p>
+        )
+    }
   }
 }
 
-export default Home;
+function mapStateToProps(state) {
+  return {
+    isAuthenticated: state.authReducer.isAuthenticated
+  }
+}
+
+export default connect(mapStateToProps)(Home)
