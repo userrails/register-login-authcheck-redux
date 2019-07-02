495da8ee10bc91113c6be84eec3d9e7819d4d9d0
Shiv Raj <shivrajbadu@gmail.com>
2019-07-02 14:23:28 +0545

    Routes added for login, register

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,7 @@ import Home from '.././components/pages/Home';
 import Contact from '.././components/pages/Contact';
 import About from '.././components/pages/About';
 import Signup from '../components/users/Register';
+import Login from '../components/users/Login';
 
 export default () => {
   return (
@@ -13,6 +14,7 @@ export default () => {
           <Route exact path="/" component={Home} />
           <Route path="/contact" component={Contact} />
           <Route path="/about" component={About} />
+          <Route exact path="/login" component={Login} />
           <Route exact path="/register" component={Signup} />
         </Switch>
       </div>
