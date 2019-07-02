11086c93c20f29ae99cc326e3d49b898cfe4e5e2
Shiv Raj <shivrajbadu@gmail.com>
2019-07-02 14:37:44 +0545

    LOGIN_USER action_type is defined

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,4 +1,5 @@
 import { REGISTER_USER } from '../actions/types';
+import { LOGIN_USER } from '../actions/types';
 
 const initialState = {
   currentUser: {},
@@ -9,6 +10,8 @@ export default function authReducer(state=initialState, action) {
   switch(action.type) {
     // loggedIn user after normal login or loggedIn after registration
     case REGISTER_USER:
+      return {...state, currentUser: action.payload, isAuthenticated: true}
+    case LOGIN_USER:
       return {...state, currentUser: action.payload, isAuthenticated: true}
     case 'LOGOUT_USER':
       return { ...state, isAuthenticated: false };
