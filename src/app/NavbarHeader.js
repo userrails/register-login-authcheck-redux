a03dae01e4e2732c5bb5562b30430e8df67909a5
Shiv Raj <shivrajbadu@gmail.com>
2019-07-02 14:31:05 +0545

    /login is mentioned in NavbarHeader

--- src/app/NavbarHeader.js
+++ src/app/NavbarHeader.js
@@ -24,6 +24,7 @@ class NavbarHeader extends Component {
           <Link to="/">Home</Link>
           <Link to="/contact">Contact</Link>
           <Link to="/register">Register</Link>
+          <Link to="/login">Login</Link>
         </div>
         )
     }
